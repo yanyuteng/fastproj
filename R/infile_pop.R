0517ae57be6ec0f3059af361ac43d76b6d9d3c1e
yanyuteng <yanyuteng123@gmail.com>
2023-07-07 14:22:37 +0800

    Push_230707c

---
+++ R/infile_pop.R
@@ -0,0 +1,5 @@
+#' Data infile_pop
+#'
+#' @author Yu-Teng Yan \email{yanyuteng@@ruc.edu.cn}
+#' @references \url{http://stats.gd.gov.cn/gdtjnj/index.html}
+"infile_pop"
---
+++ R/inparm_scene.R
@@ -0,0 +1,5 @@
+#' Data inparm_scene
+#'
+#' @author Yu-Teng Yan \email{yanyuteng@@ruc.edu.cn}
+#' @references \url{http://stats.gd.gov.cn/gdtjnj/index.html}
+"inparm_scene"
